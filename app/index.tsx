f05558f2d620179116389ad57681f2ed7096bd7d
freshwaterlemon <nguiyaksiong@gmail.com>
2025-03-24 15:24:08 +0800

    tailwindcss setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex items-center justify-center h-full"
+    >
+      <Text className="text-5xl text-primary-green">Welcome</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	// NOTE: Update this to include the paths to all of your component files.
+	content: ['./app/**/*.{js,jsx,ts,tsx}'],
+	presets: [require('nativewind/preset')],
+	theme: {
+		extend: {
+			colors: {
+				'primary-green': '#495E57',
+				'primary-yellow': '#F4CE14',
+				'secondary-coral': '#EE9972',
+				'secondary-peach': '#FBDABB',
+				'highlight-grey': '#EDEFEE',
+				'highlight-black': '#333333',
+			},
+		},
+	},
+	plugins: [],
+};
