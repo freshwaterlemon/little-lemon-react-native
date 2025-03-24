10a9eddbb229a8515309204a8d55bc038d62bcce
freshwaterlemon <nguiyaksiong@gmail.com>
2025-03-25 02:40:14 +0800

    setup update profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,8 @@
+import React, { useEffect, useState, useCallback } from 'react';
 import { View, Text, Switch, TouchableOpacity, ScrollView, Image } from 'react-native';
-import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Link, router } from 'expo-router';
+import { useFocusEffect } from '@react-navigation/native';
+import { router } from 'expo-router';
 import { ICONS } from '@/constants';
 
 const Profile = () => {
@@ -16,95 +17,67 @@ const Profile = () => {
         newsletter: false,
     });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const storedEmail = await AsyncStorage.getItem('userEmail');
-                const storedPreferences = await AsyncStorage.getItem('preferences');
-                if (storedEmail) {
-                    setEmail(storedEmail);  // Store the full email
-                    const extractedFirstName = storedEmail.split('@')[0]; // Get part before '@'
-                    setFirstname(extractedFirstName); // Set the first name
-                    await AsyncStorage.setItem('userFirstname', extractedFirstName);
+    // Fetch user data when screen is focused
+    useFocusEffect(
+        useCallback(() => {
+            const fetchData = async () => {
+                try {
+                    const storedEmail = await AsyncStorage.getItem('userEmail');
+                    const storedFirstname = await AsyncStorage.getItem('userFirstname');
+                    const storedLastname = await AsyncStorage.getItem('userLastname');
+                    const storedPhonenum = await AsyncStorage.getItem('userPhonenum');
+                    const storedPreferences = await AsyncStorage.getItem('preferences');
+
+                    if (storedEmail) setEmail(storedEmail);
+                    if (storedFirstname) setFirstname(storedFirstname);
+                    if (storedLastname) setLastname(storedLastname);
+                    if (storedPhonenum) setPhonenum(storedPhonenum);
+                    if (storedPreferences) setPreferences(JSON.parse(storedPreferences));
+                } catch (error) {
+                    console.error('Error fetching profile data:', error);
                 }
-                if (storedPreferences) {
-                    setPreferences(JSON.parse(storedPreferences)); // Parse string into an object
-                }
-            } catch (error) {
-                console.error('Error fetching email:', error);
-            }
-        };
+            };
 
-        fetchData();
-    }, []);
-
-    // Update preference state and save to AsyncStorage
-    const updateState = (key: string) => async () => {
-        const newPreferences = {
-            ...preferences,
-            [key]: !preferences[key], // Toggle the current preference
-        };
-
-        setPreferences(newPreferences); // Update the state with new preferences
-
-        try {
-            // Save updated preferences to AsyncStorage
-            await AsyncStorage.setItem('preferences', JSON.stringify(newPreferences));
-            console.log('Updated preferences saved:', newPreferences);
-        } catch (error) {
-            console.error('Error saving preferences:', error);
-        }
-    };
+            fetchData();
+        }, [])
+    );
 
     const handleUpdateProfile = () => {
-        router.push('/updateProfile'); // Navigate to the update profile screen
+        router.push('/updateProfile'); // Navigate to update profile screen
     };
 
     const handleLogOut = async () => {
         try {
             console.log('Log Out clicked');
 
-            // Clear all user data from AsyncStorage
-            await AsyncStorage.removeItem('userEmail');
-            await AsyncStorage.removeItem('userFirstname');
-            await AsyncStorage.removeItem('userLastname');
-            await AsyncStorage.removeItem('userPhonenum');
-            await AsyncStorage.removeItem('hasSeenOnboarding');
-            await AsyncStorage.removeItem('hasSeenOnboarding');
-            await AsyncStorage.removeItem('emailPreferences');
-
-            if (!email && !firstname && !lastname && !phonenum) {
-                router.replace('/onboardingScreen');
-            } else {
-                console.log('Failed to remove some user data');
-            }
-
+            // Clear user data
+            await AsyncStorage.clear();
+            router.replace('/onboardingScreen');
         } catch (error) {
             console.error('Error logging out:', error);
         }
     };
 
-
+    function updateState(key: string): void | Promise<void> {
+        throw new Error('Function not implemented.');
+    }
 
     return (
         <ScrollView className="flex-1 bg-white">
             <View className="flex-1 px-5 py-5 items-center bg-white">
                 <View className="w-full p-6 mx-8">
-                    <Text className="text-2xl font-bold">Profile Information</Text>
+                    <Text className="text-2xl font-bold mb-4">Profile Information</Text>
 
-                    {/* Avatar and Name Section */}
+                    {/* Avatar and Name */}
                     <View className="flex-row py-3 items-center">
-                        {/* Avatar Placeholder */}
                         <View className="size-20 bg-gray-300 rounded-full justify-center items-center mr-3">
                             <Text className="text-xl text-white">{firstname?.charAt(0).toUpperCase() || "?"}</Text>
                         </View>
-
-                        {/* Name */}
-                        <Text className="text-3xl font-semibold mb-5 mx-2 mt-4">{firstname || "No name found"}</Text>
-                        <Text className="text-3xl font-semibold mb-5 mt-4">{lastname || " "}</Text>
+                        <Text className="text-3xl font-semibold mx-2 mt-4">{firstname || "No name found"}</Text>
+                        <Text className="text-3xl font-semibold mt-4">{lastname || " "}</Text>
                     </View>
 
-                    {/* Display Full Email */}
+                    {/* Email and Phone */}
                     <View className="flex-cols justify-between py-3">
                         <View className="flex-row items-center">
                             <Image source={ICONS.email} className="w-6 h-6 mr-4 mt-4" />
@@ -112,61 +85,39 @@ const Profile = () => {
                         </View>
                         <View className="flex-row items-center">
                             <Image source={ICONS.phone} className="w-6 h-6 mr-4 mt-4" />
-                            <Text className="text-lg mt-4">| {phonenum || "No Phone Number"}</Text>
+                            <Text className="text-lg mt-4">| +65 {phonenum ? phonenum.replace(/(\d{4})(?=\d)/g, '$1 ') : "No Phone Number"}</Text>
                         </View>
                     </View>
-                    <View className="flex-row justify-center py-3">
-                        <TouchableOpacity
-                            className="w-full h-12 mt-4 bg-primary-green justify-center items-center rounded-lg"
-                            onPress={handleUpdateProfile}
-                        >
-                            <Text className="text-highlight-grey text-xl font-bold">Update Profile</Text>
-                        </TouchableOpacity>
-                    </View>
+
+                    {/* Update Profile Button */}
+                    <TouchableOpacity
+                        className="w-full h-12 mt-4 bg-primary-green justify-center items-center rounded-lg"
+                        onPress={handleUpdateProfile}
+                    >
+                        <Text className="text-highlight-grey text-xl font-bold">Update Profile</Text>
+                    </TouchableOpacity>
                 </View>
 
                 {/* Email Notifications */}
                 <View className="w-full p-6 mx-8">
                     <Text className="text-2xl font-bold mb-4">Email Notifications</Text>
-                    <View className="flex-row justify-between py-3">
-                        <Text className="text-lg">Order Status</Text>
-                        <Switch
-                            value={preferences.orderStatus}
-                            onValueChange={updateState('orderStatus')}
-                        />
-                    </View>
-                    <View className="flex-row justify-between py-3">
-                        <Text className="text-lg">Password Changes</Text>
-                        <Switch
-                            value={preferences.passwordChanges}
-                            onValueChange={updateState('passwordChanges')}
-                        />
-                    </View>
-                    <View className="flex-row justify-between py-3">
-                        <Text className="text-lg">Special Offers</Text>
-                        <Switch
-                            value={preferences.specialOffers}
-                            onValueChange={updateState('specialOffers')}
-                        />
-                    </View>
-                    <View className="flex-row justify-between py-3">
-                        <Text className="text-lg">Newsletter</Text>
-                        <Switch
-                            value={preferences.newsletter}
-                            onValueChange={updateState('newsletter')}
-                        />
-                    </View>
+                    {Object.entries(preferences).map(([key, value]) => (
+                        <View key={key} className="flex-row justify-between py-3">
+                            <Text className="text-lg capitalize">{key.replace(/([A-Z])/g, ' $1')}</Text>
+                            <Switch value={value} onValueChange={() => updateState(key)} />
+                        </View>
+                    ))}
                 </View>
+
+                {/* Log Out */}
                 <View className="w-full px-6 mx-8">
-                    <Text className="text-2xl color-red-500 font-bold mb-4">Danger Zone</Text>
-                    <View className="flex-row justify-center py-3">
-                        <TouchableOpacity
-                            className="w-full h-12 mt-2 bg-primary-green justify-center items-center rounded-lg"
-                            onPress={handleLogOut}
-                        >
-                            <Text className="text-highlight-grey text-xl font-bold">Log Out</Text>
-                        </TouchableOpacity>
-                    </View>
+                    <Text className="text-2xl text-red-500 font-bold mb-4">Danger Zone</Text>
+                    <TouchableOpacity
+                        className="w-full h-12 mt-2 bg-red-500 justify-center items-center rounded-lg"
+                        onPress={handleLogOut}
+                    >
+                        <Text className="text-white text-xl font-bold">Log Out</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
         </ScrollView>
--- app/updateProfile.tsx
+++ app/updateProfile.tsx
@@ -1,22 +1,165 @@
-import { View, Text } from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, Alert, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { IMAGE } from '@/constants';
 
 const UpdateProfile = () => {
     const navigation = useNavigation();
-
     // Set custom back button text
     useLayoutEffect(() => {
         navigation.setOptions({
+            headerTitle: () => (
+                <Image
+                    source={IMAGE.banner}
+                    style={{
+                        width: 120,
+                        height: 30,
+                        resizeMode: 'contain',
+                    }}
+                />
+            ),
             title: 'Edit Profile', // Set the screen title
             headerBackTitle: 'Back', // Change back button text to "Back"
         });
     }, [navigation]);
 
+    // State to hold the updated user details
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [avatar, setAvatar] = useState(null);
+
+    // Get current user data from AsyncStorage when the component mounts
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const storedFirstName = await AsyncStorage.getItem('userFirstname');
+                const storedLastName = await AsyncStorage.getItem('userLastname');
+                const storedPhone = await AsyncStorage.getItem('userPhonenum');
+                const storedAvatar = await AsyncStorage.getItem('userAvatar');
+
+                if (storedFirstName) setFirstName(storedFirstName);
+                if (storedLastName) setLastName(storedLastName);
+                if (storedPhone) setPhone(storedPhone);
+                if (storedAvatar) setAvatar(storedAvatar);
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    // Handle avatar selection
+    const pickImage = async () => {
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (permissionResult.granted === false) {
+            alert('Permission to access camera roll is required!');
+            return;
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setAvatar(result.assets[0].uri);
+        }
+    };
+
+    // Save updated profile to AsyncStorage
+    const saveProfile = async () => {
+        try {
+            // Save the updated information to AsyncStorage
+            await AsyncStorage.setItem('userFirstname', firstName);
+            await AsyncStorage.setItem('userLastname', lastName);
+            await AsyncStorage.setItem('userPhonenum', phone);
+            if (avatar) {
+                await AsyncStorage.setItem('userAvatar', avatar); // Save avatar URI
+            }
+
+            Alert.alert('Success', 'Profile updated successfully');
+            navigation.goBack(); // Navigate back to the profile screen
+        } catch (error) {
+            console.error('Error saving profile:', error);
+            Alert.alert('Error', 'Failed to update profile');
+        }
+    };
+
+    const cancel = () => {
+        navigation.goBack();
+    };
+
     return (
-        <View>
-            <Text>Update Profile</Text>
-        </View>
+        <ScrollView className="flex-1 bg-white">
+            <View className="flex-1 px-5 py-5 items-center bg-white">
+                <View className="w-full p-6 mx-8">
+                    <Text className="text-2xl font-bold text-center mb-6">Edit Profile</Text>
+                    {/* Avatar */}
+                    <View className="items-center mb-6">
+                        {avatar ? (
+                            <Image source={{ uri: avatar }} className="size-24 rounded-full mb-4" />
+                        ) : (
+                            <View className="w-24 h-24 rounded-full bg-gray-300 justify-center items-center mb-4">
+                                <Text className="text-xl text-white">?</Text>
+                            </View>
+                        )}
+                        <TouchableOpacity onPress={pickImage} className="w-1/2 h-12 mt-4 bg-primary-green justify-center items-center rounded-lg">
+                            <Text className="text-highlight-grey text-xl font-bold">Change Avatar</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    {/* First Name */}
+                    <View className="mb-4">
+                        <Text className="font-bold text-lg mb-2 ml-1">First Name</Text>
+                        <TextInput
+                            className="w-full h-12 border border-gray-300 rounded-lg px-3 mb-2 bg-white"
+                            value={firstName}
+                            onChangeText={setFirstName}
+                            placeholder="Enter your first name"
+                        />
+                    </View>
+
+                    {/* Last Name */}
+                    <View className="mb-4">
+                        <Text className="font-bold text-lg mb-2 ml-1">Last Name</Text>
+                        <TextInput
+                            className="w-full h-12 border border-gray-300 rounded-lg px-3 mb-2 bg-white"
+                            value={lastName}
+                            onChangeText={setLastName}
+                            placeholder="Enter your last name"
+                        />
+                    </View>
+
+                    {/* Phone Number */}
+                    <View className="mb-4">
+                        <Text className="font-bold text-lg mb-2 ml-1">Phone Number</Text>
+                        <TextInput
+                            className="w-full h-12 border border-gray-300 rounded-lg px-3 mb-2 bg-white"
+                            value={phone}
+                            onChangeText={setPhone}
+                            placeholder="Enter your phone number"
+                            keyboardType="phone-pad"
+                        />
+                    </View>
+
+                    {/* Save Button */}
+                    <View className='flex-col justify-between'>
+                        <TouchableOpacity onPress={saveProfile} className="w-full h-12 mt-4 bg-primary-green justify-center items-center rounded-lg">
+                            <Text className="text-white text-lg font-bold text-center">Save Changes</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={cancel} className="w-full h-12 mt-4 bg-secondary-coral justify-center items-center rounded-lg">
+                            <Text className="text-white text-lg font-bold text-center">Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
     );
 };
 
