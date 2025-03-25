d9732c607546c603233e305cd58c0cc2baf2e26b
freshwaterlemon <nguiyaksiong@gmail.com>
2025-03-25 13:31:09 +0800

    Update loginScreen.tsx

--- app/loginScreen.tsx
+++ app/loginScreen.tsx
@@ -1,6 +1,6 @@
 import { router, Stack } from 'expo-router';
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
@@ -53,43 +53,45 @@ const LoginScreen = () => {
     return (
         <>
             <Stack.Screen options={{ headerShown: false }} />
-            <View className="flex-1 justify-center items-center bg-primary-green px-5">
-                <Image
-                    source={require('../assets/images/icon.png')}
-                    className="w-1/4 h-1/6 mb-10"
-                />
-                <Text className="text-4xl font-bold mb-6 text-primary-yellow">Login</Text>
+            <ScrollView>
+                <View className="flex-1 justify-center items-center bg-primary-green px-5">
+                    <Image
+                        source={require('../assets/images/icon.png')}
+                        className="w-1/4 h-1/6 mb-10"
+                    />
+                    <Text className="text-4xl font-bold mb-6 text-primary-yellow">Login</Text>
 
-                <TextInput
-                    className="w-3/4 h-12 border border-gray-300 rounded-lg px-3 mb-5 bg-white"
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={setEmail}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
-                {emailError ? <Text className="text-red-500 font-semibold mb-4">{emailError}</Text> : null}
+                    <TextInput
+                        className="w-3/4 h-12 border border-gray-300 rounded-lg px-3 mb-5 bg-white"
+                        placeholder="Email"
+                        value={email}
+                        onChangeText={setEmail}
+                        keyboardType="email-address"
+                        autoCapitalize="none"
+                    />
+                    {emailError ? <Text className="text-red-500 font-semibold mb-4">{emailError}</Text> : null}
 
-                <TextInput
-                    className="w-3/4 h-12 border border-gray-300 rounded-lg px-3 mb-4 bg-white"
-                    placeholder="Password"
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry
-                />
-                {passwordError ? <Text className="text-red-500 font-semibold mb-5">{passwordError}</Text> : null}
+                    <TextInput
+                        className="w-3/4 h-12 border border-gray-300 rounded-lg px-3 mb-4 bg-white"
+                        placeholder="Password"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                    />
+                    {passwordError ? <Text className="text-red-500 font-semibold mb-5">{passwordError}</Text> : null}
 
-                <TouchableOpacity
-                    className="w-3/4 h-12 mt-4 bg-primary-yellow justify-center items-center rounded-lg"
-                    onPress={handleLogin}
-                >
-                    <Text className="text-highlight-black text-lg font-bold">Login</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity
+                        className="w-3/4 h-12 mt-4 bg-primary-yellow justify-center items-center rounded-lg"
+                        onPress={handleLogin}
+                    >
+                        <Text className="text-highlight-black text-lg font-bold">Login</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity className="mt-4">
-                    <Text className="text-highlight-grey text-base">Forgot Password?</Text>
-                </TouchableOpacity>
-            </View>
+                    <TouchableOpacity className="mt-4">
+                        <Text className="text-highlight-grey text-base">Forgot Password?</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </>
     );
 };
